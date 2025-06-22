3628e970f7a10c47b98463f274800000006fac59
Acdiost Chen <acdiost@gmail.com>
2025-06-22 04:38:47 +0000

    finish game

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+function Square( {value, onSquareClick} ){
+
+    return (<button 
+        className="square"
+        onClick={onSquareClick}
+    >
+        {value}
+    </button>
+    );
+}
+
+// export 关键字使此函数可以在此文件之外访问
+// default 关键字表明它是文件中的主要函数
+export default function Board() {
+
+    const [xIsNext, setXIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+    function handleClick(i){
+        // 检查方块是否被填充
+        if (squares[i] || calculateWinner(squares)){
+            return;
+        }
+        const nextSquares = squares.slice();
+        if (xIsNext){
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+    }
+
+    return (
+    // 可以使用 Fragment（<> 与 </>）包裹多个相邻的 JSX 元素
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+// 计算赢家
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
