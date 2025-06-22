dcd281ebdff902803c95c15bedfca9927a496607
Acdiost Chen <acdiost@gmail.com>
2025-06-22 05:06:48 +0000

    feat:  添加重置游戏按钮

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 
-function Square( {value, onSquareClick} ){
+function Square({ value, onSquareClick }) {
 
-    return (<button 
+    return (<button
         className="square"
         onClick={onSquareClick}
     >
         {value}
     </button>
     );
+}
+
+function ResetGame({ onResetGameClick }) {
+    return (
+        <button onClick={onResetGameClick}>Reset Game</button>
+    )
 }
 
 // export 关键字使此函数可以在此文件之外访问
@@ -25,13 +31,19 @@ export default function Board() {
         status = "Next player: " + (xIsNext ? "X" : "O");
     }
 
-    function handleClick(i){
-        // 检查方块是否被填充
-        if (squares[i] || calculateWinner(squares)){
+    // 重置游戏
+    function resetGame() {
+        setSquares(Array(9).fill(null));
+        setXIsNext(true);
+    }
+
+    function handleClick(i) {
+        // 检查格子是否被填充
+        if (squares[i] || calculateWinner(squares)) {
             return;
         }
         const nextSquares = squares.slice();
-        if (xIsNext){
+        if (xIsNext) {
             nextSquares[i] = "X";
         } else {
             nextSquares[i] = "O";
@@ -41,45 +53,47 @@ export default function Board() {
     }
 
     return (
-    // 可以使用 Fragment（<> 与 </>）包裹多个相邻的 JSX 元素
-    <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
+        // 可以使用 Fragment（<> 与 </>）包裹多个相邻的 JSX 元素
+        <>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
+            <br />
+            <ResetGame onResetGameClick={resetGame} />
+        </>
+    );
 }
 
 // 计算赢家
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
-  }
-  return null;
+    return null;
 }
